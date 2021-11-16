80731b7dabd4dbe8a85c868ac84de5debd9cbe8f
Ece Ozalp <eceiozalp@gmail.com>
2021-11-16 17:10:47 -0500

    about

--- pages/docs.tsx
+++ pages/docs.tsx
@@ -16,7 +16,7 @@ const Docs: NextPage = () => {
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 			<Header />
-			<div className="bg-gray-900">
+			<div className="bg-white dark:bg-gray-900">
 				<div className={styles.container}>
 					<div className="max-w-7xl mx-auto text-center py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
 						<h1 className="text-4xl font-extrabold tracking-tight text-white sm:text-5xl">
