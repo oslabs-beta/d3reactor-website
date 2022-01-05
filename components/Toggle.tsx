8341edb8052b53cbd6e6c26037b98532fa6b421f
irbopia <travislockett1@gmail.com>
2022-01-04 23:12:13 -0500

    Removed docusauru boiler plate and updated tailwindconfig purge

--- components/Toggle.tsx
+++ components/Toggle.tsx
@@ -1,4 +1,4 @@
-import {useState, useEffect} from 'react';
+import {useState} from 'react';
 import {Switch} from '@headlessui/react';
 import {useTheme} from 'next-themes';
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}'],
+  // purge: ['./pages/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {},
