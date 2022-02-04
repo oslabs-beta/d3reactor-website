ad35104c0eace6ac8ac734078fe99ecdf32e8d08
Ece Ozalp <eceiozalp@gmail.com>
2022-02-03 20:16:18 -0500

    changing legend location depending on the window sizE

--- components/charts/AreaChart.tsx
+++ components/charts/AreaChart.tsx
@@ -1,9 +1,37 @@
 import {AreaChart} from 'd3reactor';
 import language from '../../data/language.json';
 import {useTheme} from 'next-themes';
+import {useEffect, useState} from 'react';
+
+function useWindowSize() {
+  // Initialize state with undefined width/height so server and client renders match
+  // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
+  const [windowSize, setWindowSize] = useState({
+    width: undefined,
+    height: undefined,
+  });
+
+  useEffect(() => {
+    // only execute all the code below in client side
+    if (typeof window !== 'undefined') {
+      function handleResize() {
+        setWindowSize({
+          width: window.innerWidth,
+          height: window.innerHeight,
+        });
+      }
+      window.addEventListener('resize', handleResize);
+      handleResize();
+      return () => window.removeEventListener('resize', handleResize);
+    }
+  }, []); // Empty array ensures that effect is only run on mount
+  return windowSize;
+}
 
 export default function AreaChartExample() {
   const {theme} = useTheme();
+  const {width} = useWindowSize();
+  const cWidth = width ? width : window.innerWidth;
   return (
     <AreaChart
       height="500px"
@@ -13,11 +41,10 @@ export default function AreaChartExample() {
       yKey="popularity"
       xAxis="bottom"
       groupBy="language"
-      yAxis="left"
+      yAxis="right"
       yGrid={true}
       yAxisLabel="Popularity (%)"
-      legend="right"
-      legendLabel="Languages"
+      legend={cWidth < 900 ? 'bottom' : 'right'}
       colorScheme="schemeBlues"
       theme={theme === 'light' || theme === 'dark' ? theme : undefined}
     />
